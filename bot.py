38c8a4e1f717ceb0ec0fc26a22d4ee5f64af7a51
siredukov <siredukov@gmail.com>
2017-10-27 17:26:17 +0300

    add getmap command

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@
 import telebot
 from telebot import types
 import googlemaps
-
+import gmplot
 
 
 from processing import *
@@ -57,11 +57,12 @@ def locat(message):
         user_key = get_key(message.chat.id)
         location[get_key(message.chat.id)].append([location[user_key][-1][0] + 1, message.location.longitude, \
                                                     message.location.latitude, message.date])
-        bot.send_message(message.chat.id, "Bless you! It's your {} sneezes".format(str(location[user_key][-1][0])))
-        if location[user_key][-1][0] % 10 == 0 :
-            bot.send_sticker(message.chat.id, config.PLANTAIN_STICK) #send podorojnik sticker
+        bot.send_message(message.chat.id, "Bless you! It's your {} sneezes".format(str(location[user_key][-1][0])))        
         pickle_dump(user_key, location)
         location[user_key] = location[user_key][-10:] 
+
+        if location[user_key][-1][0] % 10 == 0 :
+            bot.send_sticker(message.chat.id, config.PLANTAIN_STICK) #send podorojnik sticker
     except:
         bot.send_message(message.chat.id, 'Try /start')
         pass  
@@ -77,10 +78,12 @@ def sneeze(message):
         location[user_key].append([location[user_key][-1][0] + 1, 'None', 'None', message.date])
                                          # add sneeze count
         bot.send_message(message.chat.id, "Bless you! It's your {} sneezes".format(str(location[user_key][-1][0])))
+        pickle_dump(user_key, location)
+        location[user_key] = location[user_key][-10:]  
+
         if location[user_key][-1][0] % 10 == 0 :
             bot.send_sticker(message.chat.id, config.PLANTAIN_STICK) #send podorojnik sticker
-        pickle_dump(user_key, location)
-        location[user_key] = location[user_key][-10:]               
+                         
     except:
         bot.send_message(message.chat.id, 'Try /start')
 
@@ -96,20 +99,22 @@ def getgeo(message):
         pass
 
     
-# @bot.message_handler(commands=["getlocation"])
-# def getlocation(message):
-#     if (get_key(message.chat.id) not in location.keys()):
-#         location[get_key(message.chat.id)] = pickle_load(get_key(message.chat.id)+'.pickle')[-10:]
-#     try:
-#         if location[get_key(message.chat.id)][-1][2] != 'None':
-#             bot.send_location(message.chat.id, location[get_key(message.chat.id)][-1][2], \
-#                                         location[get_key(message.chat.id)][-1][1])
-#         else:
-#             bot.send_message(message.chat.id, 'Your last sneeze have no location')
-
-#     except:
-#         bot.send_message(message.chat.id, 'Your last sneeze have no location')
-#         pass
+@bot.message_handler(commands=["getmap"])
+def getlocation(message):
+    coord = pickle_load(get_key(message.chat.id)+'.pickle')
+    # try:
+    gmap = gmplot.GoogleMapPlotter(59.875559, 29.827207, 16, apikey=config.GGL_API_TOKEN)
+    for c in coord:
+        lat = coord[2]
+        lon = coord[1]
+        if lat == "None" or lon == "None":
+            continue
+        gmap.scatter(lat, lon, '#3B0B39', size=40, marker=False)
+    gmap.draw("{}.html".format(get_key(message.chat.id)))
+    with open("{}.html".format(get_key(message.chat.id)), 'rb') as f:
+        bot.send_document(message.chat.id, f)
+    # except:
+        # pass
     
     
     
