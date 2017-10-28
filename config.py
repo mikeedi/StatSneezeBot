6f426622916dcf09a5a579725f85e4c6fad96f16
siredukov <siredukov@gmail.com>
2017-10-29 00:15:44 +0300

    config file -v1.0

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 """
     -*- coding: utf-8 -*-
-	Tokens
+	Tokens, api_keys, text etc.
 """
 
-BOT_TOKEN = ''
-GGL_API_TOKEN = ''
-PLANTAIN_STICK = 'CAADAgADiB4AAlOx9wNxz1H_WaIWjAI'
+BOT_TOKEN = 'Telegram bot api token'
+BOTAN_KEY = 'Yandex Metrics api key'
+PLANTAIN_STICK = 'CAADAgADiB4AAlOx9wNxz1H_WaIWjAI' # sticker key
+
+
 HELP_TEXT = 'I can very little: \n/help - some commands \n/sneeze or button - add new sneeze  \n \
-/getgeo - get history of all your sneezes with geoposition and time \n/getall - data about all sneezes \n/getmap - map with your sneezes (html file)'
+/getgeo - get history of all your sneezes with geoposition and time \n \
+/getall - data about all sneezes \n/getmap - map with your sneezes (html file)' # long text
