e6c40c1283cc6cb46419d8cfd06d1b228bdefb20
siredukov <siredukov@gmail.com>
2017-10-29 20:57:42 +0300

    Telegram Bots Analytics with Yandex Metrics.

---
+++ botan.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Taken from here https://github.com/MasterGroosha/telegram-tutorial/blob/master/lesson_06/botan.py
+
+import requests
+import json
+
+TRACK_URL = 'https://api.botan.io/track'
+
+def make_json(message):
+    data = {}
+    data['message_id'] = message.message_id
+    data['from'] = {}
+    data['from']['id'] = message.from_user.id
+    if message.from_user.username is not None:
+        data['from']['username'] = message.from_user.username
+    data['chat'] = {}
+    data['chat']['id'] = message.chat.id
+    return data
+
+
+def track(token, uid, message, name='Message'):
+    try:
+        r = requests.post(
+            TRACK_URL,
+            params={"token": token, "uid": uid, "name": name},
+            data=make_json(message),
+            headers={'Content-type': 'application/json'},
+        )
+        return r.json()
+    except requests.exceptions.Timeout:
+        # set up for a retry, or continue in a retry loop
+        return False
+    except (requests.exceptions.RequestException, ValueError) as e:
+        # catastrophic error
+        print(e)
+        return False
