eed08cac46a7a00d95c4025e031eb8b76cdb56bb
siredukov <siredukov@gmail.com>
2017-10-31 23:40:23 +0300

    config -v1.0

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ PLANTAIN_STICK = 'CAADAgADiB4AAlOx9wNxz1H_WaIWjAI' # sticker key
 
 HELP_TEXT = 'I can very little: \n/help - some commands \n/sneeze or button - add new sneeze  \n \
 /getgeo - get history of all your sneezes with geoposition and time \n \
-/getall - data about all sneezes \n' # long text
+/getall - data about all sneezes \n \
+@siredukov - give feedback' # long text
