ab910fe7813535e38d67d05f8ecddd41f0bd04e9
siredukov <siredukov@gmail.com>
2017-11-05 14:41:45 +0300

    fixed

--- bot.py
+++ bot.py
@@ -60,36 +60,35 @@ location = {}   #data store dictionary
 # get information about all commands
 @bot.message_handler(commands=["help"])
 def helpme(message):
-    bot.send_message(message.chat.id, config.HELP_TEXT, parse_mode='HTML')  
+    bot.send_message(message.chat.id, config.HELP_TEXT, parse_mode='HTML')   # Help text is contained in config.py
     botan.track(config.BOTAN_KEY, message.chat.id, message, '/help')
    
     
 # initialize user 
 @bot.message_handler(commands=["start"])
 def start(message):
-    if exist("pickles/{}.pickle".format(get_key(message.chat.id))):
+    if exist("pickles/{}.pickle".format(get_key(message.chat.id))):   # use piclkes file instead database
         bot.send_message(message.chat.id, "You have already started")
     else:
-        location[get_key(message.chat.id)] = [[0]]
+        location[get_key(message.chat.id)] = [[0]]  # initialize new user
         bot.send_message(message.chat.id, "Hi. I'm a bot that collect sneezes statistics")
-        helpme(message)
-
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_geo = types.KeyboardButton(text="SNEEZED!! !", request_location=True)
-    keyboard.add(button_geo)
-    bot.send_message(message.chat.id, "Tap when sneezed", reply_markup=keyboard)
+        helpme(message)  # send command description
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) # button class
+    button_geo = types.KeyboardButton(text="SNEEZED!! !", request_location=True) # button
+    keyboard.add(button_geo) # create button
+    bot.send_message(message.chat.id, "Tap when sneezed", reply_markup=keyboard) # add button
     botan.track(config.BOTAN_KEY, message.chat.id, message, '/start')
  
 # sneeze with location
 @bot.message_handler(content_types=['location'])
 def locat(message):
-    user_key = get_key(message.chat.id)
-    if user_key not in location.keys():
-        location[user_key] = pickle_load(user_key)[-5:]
+    user_key = get_key(message.chat.id)  # get_key return hash string from user id
+    if user_key not in location.keys():  # load last 5 location
+        location[user_key] = pickle_load(user_key)[-5:] # it is need if program will failed
     location[user_key].append([location[user_key][-1][0] + 1, message.location.longitude, \
-                                                message.location.latitude, message.date])
+                                                message.location.latitude, message.date])  # add sneeze count
     bot.send_message(message.chat.id, "Bless you! It's your {} sneezes".format(str(location[user_key][-1][0])))        
-    pickle_dump(user_key, location[get_key(message.chat.id)][-1])
+    pickle_dump(user_key, location[get_key(message.chat.id)][-1])  # after recording rewrite to pickles
     location[user_key] = pickle_load(user_key)[-5:] 
     if location[user_key][-1][0] % 10 == 0 :
         bot.send_sticker(message.chat.id, config.PLANTAIN_STICK) #send podorojnik sticker
